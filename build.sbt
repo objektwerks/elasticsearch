cac5c9b7ce5239f9cf6b4d1d5f5349effd1f28e9
objektwerks <objektwerks@runbox.com>
2021-03-26 08:04:46 -0400

    attempting build a simple test for the lastest elastic4s version

--- build.sbt
+++ build.sbt
@@ -6,6 +6,7 @@ libraryDependencies ++= {
   val elastic4sVersion = "7.12.0"
   Seq(
     "com.sksamuel.elastic4s" %% "elastic4s-core" % elastic4sVersion,
+    "com.sksamuel.elastic4s" %% "elastic4s-http" % "6.7.8",
     "ch.qos.logback" % "logback-classic" % "1.2.3",
     "com.sksamuel.elastic4s" %% "elastic4s-testkit" % elastic4sVersion % Test,
     "org.scalatest" %% "scalatest" % "3.2.6" % Test
--- src/test/scala/objektwerks/Elastic4sTest.scala
+++ src/test/scala/objektwerks/Elastic4sTest.scala
@@ -1,10 +1,26 @@
 package objektwerks
 
+import com.sksamuel.elastic4s.http.ElasticDsl._
+import com.sksamuel.elastic4s.http.search.SearchResponse
+import com.sksamuel.elastic4s.http._
+import com.sksamuel.elastic4s.requests.common.RefreshPolicy
+
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.must.Matchers
 
 class Elastic4sTest extends AnyFunSuite with Matchers {
   test("search") {
-    println("2021.3.24 - TODO!")
+    val client = ElasticClient(ElasticProperties("http://objektwerks.local:9200"))
+
+    client.execute {
+      indexInto("bands" / "artists") fields ("name" -> "coldplay") refresh (RefreshPolicy.WAIT_FOR)
+    }.await
+
+    val response: Response[SearchResponse] = client.execute {
+      search("bands") matchQuery("name", "coldplay")
+    }.await
+
+    println(response.result.hits.hits.head.sourceAsString)
+    client.close()
   }
 }
