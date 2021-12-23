3823e417071c371bbc5c7e9db23e3ca52a7d4eb6
objektwerks <objektwerks@runbox.com>
2021-12-23 09:51:01 -0500

    refactored elastic4s test

--- src/test/scala/objektwerks/Elastic4sTest.scala
+++ src/test/scala/objektwerks/Elastic4sTest.scala
@@ -2,6 +2,7 @@ package objektwerks
 
 import com.sksamuel.elastic4s.ElasticDsl._
 import com.sksamuel.elastic4s.{ElasticClient, ElasticProperties}
+import com.sksamuel.elastic4s.fields.TextField
 import com.sksamuel.elastic4s.http.JavaClient
 import com.sksamuel.elastic4s.requests.common.RefreshPolicy
 
@@ -15,21 +16,31 @@ class Elastic4sTest extends AnyFunSuite with Matchers {
   val url = conf.getString("url")
 
   test("search") {
-    val properties = ElasticProperties(url)
-    val client = ElasticClient( JavaClient(properties) )
+    val elasticProps = ElasticProperties(url)
+    val client = ElasticClient( JavaClient(elasticProps) )
 
+    // created index
+    client.execute {
+      createIndex("artists").mapping(
+        properties(
+          TextField("name")
+        )
+      )
+    }.await
+
+    // index into
     client.execute {
       indexInto("artists")
-        .fields ("name" -> "coldplay")
-        .refresh (RefreshPolicy.WAIT_FOR)
+        .fields("name" -> "led zeppelin")
+        .refresh(RefreshPolicy.WAIT_FOR)
     }.await
 
+    // search
     val response = client.execute {
-      search("artists")
-        .matchQuery("name", "coldplay")
+      search("artists").matchQuery("name", "led zeppelin")
     }.await
-
     println(response.result.hits.hits.head.sourceAsString)
+
     client.close()
   }
 }
